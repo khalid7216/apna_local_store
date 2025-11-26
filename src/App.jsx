1ebb0acc8e9c6405030497bc57175a1c7d12dc4a
khalid7216 <khalidsanawer2@gmail.com>
2025-11-26 16:28:26 +0500

    changs

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import Navbar from "./component/Navbar.jsx";
 import Banner from "./component/Banner.jsx";
+import Navbar from './component/navbar.jsx';
 
 export default function App() {
   return (
     <>
-  <Navbar/>
-    <Banner/>
+      <Navbar />
+      <Banner />
     </>
   )
 }
